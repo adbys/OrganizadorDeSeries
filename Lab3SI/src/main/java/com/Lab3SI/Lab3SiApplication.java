3e74cfdf327384272a8860060d264639ebd3e9c9
Adbys Vasconcelos <adbys.andrade@ccc.ufcg.edu.br>
2017-07-09 16:31:18 -0300

    iniciando backend

---
+++ Lab3SI/src/main/java/com/Lab3SI/Lab3SiApplication.java
@@ -0,0 +1,12 @@
+package com.Lab3SI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3SiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3SiApplication.class, args);
+	}
+}
---
+++ Lab3SI/src/test/java/com/Lab3SI/Lab3SiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Lab3SI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Lab3SiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
