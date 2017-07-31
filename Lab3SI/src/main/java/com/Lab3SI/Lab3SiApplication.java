789c2ca3ae5119de1a3381bb103551728ef531f2
Adbys José Vasconcelos de Andrade <adbys.andrade@ccc.ufcg.edu.br>
2017-07-30 21:19:43 -0300

    Update Lab3SiApplication.java

--- Lab3SI/src/main/java/com/Lab3SI/Lab3SiApplication.java
+++ Lab3SI/src/main/java/com/Lab3SI/Lab3SiApplication.java
@@ -2,9 +2,10 @@ package com.Lab3SI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Lab3SiApplication {
+public class Lab3SiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lab3SiApplication.class, args);
